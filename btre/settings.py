3910a7ecfbed379105402cd0e9a9edef03e1343d
Ling Jin <lingjin@Lings-MacBook-Pro.local>
2019-09-29 00:01:48 -0700

    Finish the project: contact inquiries

--- btre/settings.py
+++ btre/settings.py
@@ -140,3 +140,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'  
 }
+
+# Email config 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER='lingj6@uci.edu'
+EMAIL_HOST_PASSWORD='Lynne156527'
+EMAIL_USE_TLS=True
