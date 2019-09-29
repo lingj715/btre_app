de9d1fbfbb650a1c60e3431307bb2e848e7b332d
Ling Jin <lingjin@Lings-MacBook-Pro.local>
2019-09-29 01:35:26 -0700

    Added local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -147,3 +147,9 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER='lingj6@uci.edu'
 EMAIL_HOST_PASSWORD='Lynne156527'
 EMAIL_USE_TLS=True
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
